8482b98d9f0a08a339f6a5eb2f12c87241a8bb3c
Abhijit Parida <abhijitparida.me@gmail.com>
2018-02-20 01:59:25 +0530

    api-test: Add user input

--- api-test/search_request.py
+++ api-test/search_request.py
@@ -1,18 +1,26 @@
 import pystache
 import requests
 from requests.auth import HTTPBasicAuth
+import datetime
+
+country_code = input('Country code (default - DE): ') or 'DE'
+departure = input('From (default - ADA): ') or 'ADA'
+arrival = input('To (default - AYT): ') or 'AYT'
+date = input('Departure Date (default - today): ') or datetime.date.today().strftime('%Y-%m-%d')
+travelers = input('Travelers (default - 1): ') or '1'
+max_connections = input('Max connections (default - 1): ') or '1'
 
 with open('templates/SearchRequest.xml', 'r') as f:
     template = f.read()
 
 values = {
-    'country_code': 'DE',
+    'country_code': country_code,
     'agent_user_id': 'HKTHONUSR',
-    'travellers': '1',
-    'departure': 'ADA',
-    'date': '2018-02-22',
-    'arrival': 'AYT',
-    'max_connections': '1'
+    'travelers': travelers,
+    'departure': departure,
+    'date': date,
+    'arrival': arrival,
+    'max_connections': max_connections
 }
 payload = pystache.render(template, values)
 
