26eefcadf84d42342e2893f042390a1dae6516d2
SUD <sudhansu.ace@gmail.com>
2018-02-24 16:52:55 +0530

    Templates Added - home, index, profile

--- iatadjango/iatadjango/urls.py
+++ iatadjango/iatadjango/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
@@ -23,3 +25,9 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
---
+++ iatadjango/static/js/script.js
@@ -0,0 +1,22 @@
+function flight_status() {
+    var x = document.getElementById('status');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    }else {
+        x.style.display = 'none';
+    }
+}
+function increaseValue() {
+  var value = parseInt(document.getElementById('number').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value++;
+  document.getElementById('number').value = value;
+}
+
+function decreaseValue() {
+  var value = parseInt(document.getElementById('number').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value < 1 ? value = 1 : '';
+  value--;
+  document.getElementById('number').value = value;
+}
